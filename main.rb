abe5909b04ee6875faf57b8023be6abdaa1b8b11
crazyief <crazyief@gmail.com>
2014-11-22 19:40:04 +0800

    half way

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ helpers do
     total_number=0
 
     arr.each do |number|
-      if number=="Ace"
+      if number=="ace"
         number=11
       else
           number=10 if number.to_i==0
@@ -18,7 +18,7 @@ helpers do
       total_number=total_number+number
     end # end of each do 
 
-    new_arr=arr.select{|element| element=="Ace"}
+    new_arr=arr.select{|element| element=="ace"}
     ace_count=new_arr.size
     
     ace_count.times {
@@ -27,11 +27,18 @@ helpers do
     }
     total_number
   end
+
+  def card_image(card)
+    #{}"img src='/images/cards/#{card[0].to_s}_#{card[1].to_s}.jpg'"
+    "<img src='/images/cards/#{card[0]}_#{card[1]}.jpg' class='card_image'>"
+  end
 end
 
 before do
   @show_hit_or_stay_btn=true
 end
+
+#===========
 
 get "/"  do
   if session[:player_name]
@@ -41,20 +48,9 @@ get "/"  do
   end
 end
 
-get "/new_player" do
-  erb :new_player
-end
-
-post "/new_player" do
-  session[:player_name]= params[:player_name]
-  session[:money]= params[:money]
-  redirect "/game"
-end
-
-
 get '/game' do
-  color=%w(Spade Club Heart Square)
-  rank=%w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King)
+  color=%w(spades clubs hearts diamonds)
+  rank=%w(ace 2 3 4 5 6 7 8 9 10 jack queen king)
   session[:deck]=color.product(rank).shuffle!
 
   session[:dealer_card]=[]
@@ -68,14 +64,34 @@ get '/game' do
   erb :game
 end
 
+get "/new_player" do
+  erb :new_player
+end
+
+post "/new_player" do
+  if params[:player_name].empty?
+    @error="Name is require"
+    halt erb(:new_player)
+  end
+
+  session[:player_name]=params[:player_name]
+  session[:money]=params[:money]
+  redirect "/game"
+end
+
 
 post "/game/player/hit" do
   session[:player_card]<<session[:deck].pop
 
-  if calculate_total(session[:player_card])>21
+  player_score=calculate_total(session[:player_card])
+  if player_score>21
     @error="Busted !!  >21 "
     @show_hit_or_stay_btn=false
+  elsif player_score==21 
+    @success="You get 21 ! Well done "
+    @show_hit_or_stay_btn=false
   end
+
   erb :game
 end
 
@@ -84,7 +100,3 @@ post "/game/player/stay" do
   @show_hit_or_stay_btn=false
   erb :game
 end
-
-
-
-
